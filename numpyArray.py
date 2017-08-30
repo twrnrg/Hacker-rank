a6f7cf32e4bd263f4b375c5c0ef4d065d4ece5dd
Anurag Tiwari <nrgvmltwr@gmail.com>
2017-08-31 00:58:20 +0530

    numpy array

---
+++ numpyArray.py
@@ -0,0 +1,11 @@
+# python- printing reverse numPy array
+import numpy
+def arrays(arr):
+    def arrays(arr):
+    b=numpy.array(arr, float)
+    c=b[::-1]
+    return c
+        
+    arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
